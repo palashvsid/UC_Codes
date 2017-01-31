3a0c6c1309bc39654607ba84f930b4ccbbbe32fb
palashvsid <palashv.sid@gmail.com>
2017-01-30 19:02:57 -0500

    Data Mining I- Assignment 2- Final Code

--- Data Mining I/Assignment_2.R
+++ Data Mining I/Assignment_2.R
@@ -17,16 +17,20 @@ y_no_error = beta0 + beta1*x1 + beta2*x2
 data <- data.frame(y_no_error=y_no_error, x1 =x1, x2=x2)
 
 # Part ii
-error <- rnorm(m, mean=0, sd= 1)
-df_beta <- data.frame(beta0=c(), beta1= c(), beta2= c()) 
+df_beta <- data.frame(beta0=double(), beta1= double(), beta2= double()) 
 vec_MSE = c()
 
 for (i in 1:m)
 {
-  data$y = data$y_no_error+error[i]
+  error <- rnorm(n, mean=0, sd= 1)
+  data$y = data$y_no_error+error
   lm.out=lm(y~x1+x2, data=data)
-  df_beta[j,]= lm.out$coefficients
+  df_beta[i,]= lm.out$coefficients
   mysummary= summary(lm.out)
-  vec_MSE[j]= mysummary$sigma^2
-  
+  vec_MSE[i]= mysummary$sigma^2
 }
+
+beta_mean = apply(df_beta, 2, mean)
+beta_bias = beta_mean - c(beta0, beta1, beta2)
+beta_var = apply(df_beta, 2, var)
+mse = beta_bias*beta_bias + beta_var
